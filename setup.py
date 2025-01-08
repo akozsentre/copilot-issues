5bf3c3c378617fe88a95a2f2ca7566a16133ff54
akozsentre <akozsentre@users.noreply.github.com>
2025-01-08 09:46:30 +0000

    Update: Introduces plugin architecture for extensibility

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from setuptools import setup, find_packages\n\nsetup(\n    name='copilot-issues',\n    version='1.0.0',\n    packages=find_packages(),\n    install_requires=[]\n)
 
 # PR Merge: 2025-10-23 - feature/merge-4438
+
+# PR Update: 2025-10-23 - fix/update-5626
